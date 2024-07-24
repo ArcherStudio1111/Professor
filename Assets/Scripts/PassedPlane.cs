411cdbe35d9c35eb883fe6d6b07c59fca8e8bbf6
ArcherStudio1111 <archerstudio666666@gmail.com>
2024-07-24 19:27:29 +0800

    Update PassedPlane.cs

--- Assets/Scripts/PassedPlane.cs
+++ Assets/Scripts/PassedPlane.cs
@@ -15,6 +15,12 @@ public class PassedPlane : MonoBehaviour
     {
         if (other.CompareTag("Block"))
         {
+            var experimentObject = FindObjectOfType<ExperimentObject>();
+            if (experimentObject.blockPassStatus != ExperimentObject.BlockPassStatus.Passed)
+            {
+                experimentObject.blockPassStatus = ExperimentObject.BlockPassStatus.OutBound;
+            }
+                
             if (judgeCombine.Equals(JudgeCombine.IsCombinedBlock))
             {
                 other.transform.parent.parent.GetComponent<Block>().InvokeBlockFinishEvent();
