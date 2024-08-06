ac02edda4f5d464543be1d8926187a1488db2aab
ArcherStudio1111 <archerstudio666666@gmail.com>
2024-08-06 15:24:13 +0800

    Fix multiple blocks outbound-obstructed bug

--- Assets/Scripts/PassedPlane.cs
+++ Assets/Scripts/PassedPlane.cs
@@ -15,18 +15,22 @@ public class PassedPlane : MonoBehaviour
     {
         if (other.CompareTag("Block"))
         {
-            var experimentObject = FindObjectOfType<ExperimentObject>();
-            if (experimentObject.blockPassStatus != ExperimentObject.BlockPassStatus.Passed)
-            {
-                experimentObject.blockPassStatus = ExperimentObject.BlockPassStatus.OutBound;
-            }
-                
             if (judgeCombine.Equals(JudgeCombine.IsCombinedBlock))
             {
+                var experimentObject = other.transform.parent.parent.parent.parent.GetComponent<ExperimentObject>();
+                if (experimentObject.blockPassStatus != ExperimentObject.BlockPassStatus.Passed)
+                {
+                    experimentObject.blockPassStatus = ExperimentObject.BlockPassStatus.OutBound;
+                }
                 other.transform.parent.parent.GetComponent<Block>().InvokeBlockFinishEvent();
             }
             else if (judgeCombine.Equals(JudgeCombine.IsNotCombinedBlock))
             {
+                var experimentObject = other.transform.parent.parent.GetComponent<ExperimentObject>();
+                if (experimentObject.blockPassStatus != ExperimentObject.BlockPassStatus.Passed)
+                {
+                    experimentObject.blockPassStatus = ExperimentObject.BlockPassStatus.OutBound;
+                }
                 other.GetComponent<Block>().InvokeBlockFinishEvent();
             }
         }
